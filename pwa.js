a1defecc38e15a0eef68ec16e57ece323d2b356f
andywu10504 <66308965+andywu10504@users.noreply.github.com>
2025-11-11 14:08:10 +0800

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('sw.js')
+      .register('sw.js', { scope: './' })
       .then(() => console.log('✅ Service Worker registered'))
       .catch(err => console.error('SW registration failed:', err));
   });
